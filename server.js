46cb5dd91a64d99c97d446094383ba58f4998bf1
Abuwa Ofeghareno <eghareabuwa@gmail.com>
2026-05-21 12:05:42 +0100

    Trigger Azure deployment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello! My Node.js app is running successfully on Azure App Service.");
+  res.send("Hello! My Node.js app has been deployed through GitHub Action to Azure App Service");
 });
 
 app.listen(PORT, () => {
