18c10f4cbc44c110d262591c417717560f55f529
Abuwa Ofeghareno <eghareabuwa@gmail.com>
2026-05-21 12:28:49 +0100

    Redeploy Node app after rntime update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello! My Node.js app has been deployed through GitHub Action to Azure App Service");
+  res.send("Hello! My Node app.js app is now running from GitHub Actions on Azure App Service.");
 });
 
 app.listen(PORT, () => {
